7bf792a7eb88e6749d34826c43e866ef2079d0aa
Galina Slavova <galina@crafity.com>
2013-08-09 15:36:35 +0200

    work in progress - unit tests

--- test/resources.test.js
+++ test/resources.test.js
@@ -23,6 +23,52 @@ var jstest = require('crafity-jstest').createContext("Crafity Resources Tests")
 
 jstest.run({
 
+//	"TEST #1": function (test) {
+//		test.async(2000); // give the timeout for this unit test to complete
+//
+//		// arrange
+//		var resources = require('../main.js');
+//
+//		// act
+//		resources.configure({ path: "/resources1", defaultLanguage: "nl"}, function (err, resourcesObject) {
+//				console.log("err", err);
+//				console.log("resourcesObject", resourcesObject);
+//				
+//				test.complete();
+//			}
+//		);
+//	},
+//	"TEST #2": function (test) {
+//		test.async(2000); // give the timeout for this unit test to complete
+//
+//		// arrange
+//		var resources = require('../main.js');
+//
+//		// act
+//		resources.configure({ path: "/resources2", defaultLanguage: "en"}, function (err, resourcesObject) {
+//				console.log("err", err);
+//				console.log("resourcesObject", resourcesObject);
+//				
+//				test.complete();
+//			}
+//		);
+//	},
+//	"TEST #3": function (test) {
+//		test.async(2000); // give the timeout for this unit test to complete
+//
+//		// arrange
+//		var resources = require('../main.js');
+//
+//		// act
+//		resources.configure(function (err, resourcesObject) {
+//				console.log("err", err);
+//				console.log("resourcesObject", resourcesObject);
+//				
+//				test.complete();
+//			}
+//		);
+//	},
+
 	"#1 When requiring the resources module Then a result of type Function is returned.": function () {
 		var resources = require('../main.js');
 
@@ -30,7 +76,46 @@ jstest.run({
 		assert.isFunction(resources.configure, "Expected to have configure function.");
 	},
 
-	"#2 When calling configure method with no options and existing '~/resources/en.json' path Then default language is English.": function (test) {
+	"#2 When calling configure method with no arguments Then an error is returned.": function () {
+		// arrange
+		var resources = require('../main.js');
+
+		assert.isDefined(resources, "Expected resources to be defined.");
+		// act
+
+		try {
+			resources.configure();
+			assert.fail("Expected an exception to be thrown.");
+		} catch (err) {
+
+			console.log("err", err);
+
+			// assert
+			assert.isDefined(err, "Expected err to be defined.");
+			assert.isNotNull(err, "Expected err to be defined.");
+
+		}
+
+	},
+	"#99 When calling constructor ResourcesAgent Then ...": function () {
+// arrange
+		var resources = require('../main.js');
+
+		try {
+			new resources.ResourcesAgent().get();
+			assert.fail("Expected an exception to be thrown.");
+		}
+		catch (err) {
+			console.log("err", err);
+			
+			assert.isDefined(err, "Expected err to be defined.");
+			assert.areEqual("Expected argument language.", err.message, "Expected err error messaged to be the same.");
+			assert.isNotNull(err, "Expected err to be defined.");
+
+		}
+	},
+
+	"#3 When calling configure method with no options and existing '~/resources/en.json' path Then default language is English.": function (test) {
 		test.async(2000); // give the timeout for this unit test to complete
 
 		// arrange
@@ -42,7 +127,7 @@ jstest.run({
 		// act
 		resources.configure(function (err, resourcesObject) {
 				// assert
-				assert.isNull(err, "Expected that err should be null.");
+				assert.isNull(err, "Expected that no error should be thrown.");
 				assert.isDefined(resourcesObject, "Expected resources result to be defined.");
 
 				assert.areEqual(1, resourcesObject.getLanguages().length, "Expected the resources to have one default language.");
@@ -53,24 +138,35 @@ jstest.run({
 		);
 	},
 
-	"#3 When calling configure method with a invalidly formatted json file as an argument Then an error is returned.": function (test) {
-		test.async(2000); // give the timeout for this unit test to complete
+	"#4 When calling configure method with an invalidly formatted json file as an argument Then an error is returned.": function (test) {
+		test.async(1000); // give the timeout for this unit test to complete
 
 		// arrange
 		var resources = require('../main.js');
 
+		assert.isDefined(resources, "Undefined resoucres");
+
 		// act
-		resources.configure({ path: '/resources1', "defaultLanguage": "nl"}, function (err) {
+		try {
+
+			resources.configure({ path: '/resources1', "defaultLanguage": "nl" }, function (err) {
+				console.log("#3");
+				console.log("\nerr = ", err);
+
 				// assert
+				assert.isDefined(err, "Expected err to be defined.");
 				assert.isNotNull(err, "Expected err to be defined.");
 
 				test.complete();
-			}
-		);
+			});
+
+		} catch (ex) {
+			console.log("ex", ex);
+		}
 
 	},
 
-	"#4 When calling configure method with a custom file path with no files underneath Then an error is returned.": function (test) {
+	"#5 When calling configure method with a custom file path with no files underneath Then an error is returned.": function (test) {
 		test.async(2000); // give the timeout for this unit test to complete
 
 		// arrange
@@ -86,40 +182,33 @@ jstest.run({
 			}
 		);
 	},
+//
+//	"#6 When defining a crafity namespace file under 'resources' folder Then the result json contains two nested objects with namespaces as keys.": function (test) {
+//		test.async(2000);
+//
+//		// arrange
+//		var resources = require('../main.js');
+//
+//		// act
+//		resources.configure(function (err, resourcesResult) {
+//				console.log("err", err);
+//				console.log("\nresourcesResult= ", resourcesResult);
+//
+//				var crafityResources = resourcesResult.getResources("en", "crafity").get()
+//					, defaultResources = resourcesResult.getResources("en", "default").get()
+//					;
+//
+//				console.log("crafityResources = ", crafityResources);
+//				console.log("defaultResources = ", defaultResources);
+//
+//				assert.isDefined(crafityResources, "Expected crafityResources to be defined.");
+//				assert.isDefined(defaultResources, "Expected defaultResources to be defined.");
+//
+//				test.complete();
+//			}
+//		);
+//	}
 
-	"#5 When defining a crafity namespace file under 'resources' folder Then the result json contains two nested objects with namespaces as keys.": function (test) {
-		test.async(2000);
-
-		// arrange
-		var resources = require('../main.js');
-
-		// act
-		resources.configure(function (err, resourcesResult) {
-				console.log("err", err);
-				console.log("\nresourcesResult= ", resourcesResult);
-
-				var crafityResources = resourcesResult.getResources("en", "crafity").get()
-					, defaultResources = resourcesResult.getResources("en", "default").get()
-					;
-				
-				console.log("crafityResources = ", crafityResources);
-				console.log("defaultResources = ", defaultResources);
-
-				assert.isDefined(crafityResources, "Expected crafityResources to be defined.");
-				assert.isDefined(defaultResources, "Expected defaultResources to be defined.");
-
-				test.complete();
-			}
-		);
-	}
-
-	
-	// resourcesModule.configure(options, function(err, resourcesAgent){
-	
-//	resourcesAgent.getResources 
-//	resourcesAgent.get => return the json formatted value of the resources key stirng
-
-//});
 });
 
 
